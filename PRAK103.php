447d28caa148f9e6ce4bbf6b9cb62224db2a9510
biahlil <2010817110011@mhs.ulm.ac.id>
2022-03-15 15:34:37 +0800

    Tugas Praktikum Web 2 15/03/2022

--- PRAK103.php
+++ PRAK103.php
@@ -3,7 +3,7 @@
     $Fahrenheit = ($celcius *9/5) + 32;
     $Reamur = $celcius *4/5;
     $Kelvin = $celcius + 273.15;
-    echo "Fahrenheit (F) = ".number_format($Fahrenheit,3,",") . "</br>";
-    echo "Reamur (R) = ".number_format($Reamur,3,","). "</br>";
-    echo "Kelvin (K) = ".number_format($Kelvin,3,","). "</br>";
+    echo "Fahrenheit (F) = ".number_format($Fahrenheit,4,",") . "</br>";
+    echo "Reamur (R) = ".number_format($Reamur,4,","). "</br>";
+    echo "Kelvin (K) = ".number_format($Kelvin,4,","). "</br>";
 ?>
